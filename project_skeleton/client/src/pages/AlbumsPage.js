9e4dbef2d988f82338db62a563f8add4910b0abf
JLmm123 <114846480+JLmm123@users.noreply.github.com>
2024-05-05 02:02:40 -0400

    Update AlbumsPage.js

--- project_skeleton/client/src/pages/AlbumsPage.js
+++ project_skeleton/client/src/pages/AlbumsPage.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Box, Container } from '@mui/material';
 import { NavLink } from 'react-router-dom';
-
+import LazyTable from '../components/LazyTable';
 const config = require('../config.json');
 
 export default function AlbumsPage() {
@@ -21,10 +21,38 @@ export default function AlbumsPage() {
   const format3 = { display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly' };
   const format4 = { display: 'grid', justifyContent: 'space-evenly' };
 
+  const neighborListColumns = [
+    {
+      field: 'neighborhood',
+      headerName: 'Neighborhood',
+      renderCell: (row) => <NavLink to={`/airbnb_list/${row.neighborhood}`}>{row.neighborhood}</NavLink>
+    },
+    {
+      field: 'listing_count',
+      headerName: 'Total Numbers of Airbnb Listings'
+    },
+  ];
+
   return (
     // TODO (TASK 22): Try out the different provided formatting options by replacing “format1”  in the Container's style property with the other provided options.
     // TODO (TASK 22): Choose the one that displays all the albums in a fluid grid.
-    <Container style={format1}>
+    <>
+    <iframe 
+      src="https://www.google.com/maps/d/embed?mid=1HwsLepsrnWEI1Hxi-ivdp1WSjHI&hl=en&ehbc=2E312F" 
+      width="1280" 
+      height="480"
+      title="Google Map"
+      style={{ display: 'block', margin: 'auto' }} // Center the iframe horizontally
+    ></iframe>
+    
+   
+  <LazyTable
+        route={`http://${config.server_host}:${config.server_port}/neighborhood`}
+        columns={neighborListColumns}
+        defaultPageSize={10} 
+        rowsPerPageOptions={[10, 20]}/>
+
+    {/* <Container style={format1}>
       {neighborhood.map((neighborhood) =>
         <Box
           key={neighborhood.neighborhood}
@@ -32,14 +60,15 @@ export default function AlbumsPage() {
           m={2}
           style={{ background: '#c5cae9', borderRadius: '16px', border: '2px solid #000' }}
         >
-          {/* <img
+          <img
             key={neighborhood.neighborhood}
             src={neighborhood.thumbnail_url}
             alt={`${neighborhood.neighborhood}`}
-          /> */}
+          />
           <h4><NavLink to={`/airbnb_list/${neighborhood.neighborhood}`}>{neighborhood.neighborhood}</NavLink></h4> 
         </Box>
       )}
-    </Container>
+    </Container> */}
+    </>
   );
 }
