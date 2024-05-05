72726169c7ee5d1ea4e84d8a3ccd79d43a032f11
HeidiHe2001 <ccheziqi@gmail.com>
2024-05-05 12:16:34 -0400

    update albumpage to neighborhood page

--- project_skeleton/client/src/pages/NeighborhoodPage.js
+++ project_skeleton/client/src/pages/NeighborhoodPage.js
@@ -13,14 +13,6 @@ export default function AlbumsPage() {
       .then(resJson => setNeighborhood(resJson));
   }, []);
 
-  // These formatting options leverage flexboxes, an incredibly powerful tool for formatting dynamic layouts.
-  // You can learn more on MDN web docs linked below (or many other online resources)
-  // https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Flexbox
-  const format1 = {};
-  const format2 = { display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' };
-  const format3 = { display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly' };
-  const format4 = { display: 'grid', justifyContent: 'space-evenly' };
-
   const neighborListColumns = [
     {
       field: 'neighborhood',
@@ -34,22 +26,19 @@ export default function AlbumsPage() {
   ];
 
   return (
-    // TODO (TASK 22): Try out the different provided formatting options by replacing “format1”  in the Container's style property with the other provided options.
-    // TODO (TASK 22): Choose the one that displays all the albums in a fluid grid.
     <>
-    <iframe 
-      src="https://www.google.com/maps/d/embed?mid=1HwsLepsrnWEI1Hxi-ivdp1WSjHI&hl=en&ehbc=2E312F" 
-      width="1280" 
+    <iframe
+      src="https://www.google.com/maps/d/embed?mid=1HwsLepsrnWEI1Hxi-ivdp1WSjHI&hl=en&ehbc=2E312F"
+      width="1280"
       height="480"
       title="Google Map"
       style={{ display: 'block', margin: 'auto' }} // Center the iframe horizontally
     ></iframe>
-    
-   
+
   <LazyTable
         route={`http://${config.server_host}:${config.server_port}/neighborhood`}
         columns={neighborListColumns}
-        defaultPageSize={10} 
+        defaultPageSize={10}
         rowsPerPageOptions={[10, 20]}/>
 
     {/* <Container style={format1}>
@@ -65,7 +54,7 @@ export default function AlbumsPage() {
             src={neighborhood.thumbnail_url}
             alt={`${neighborhood.neighborhood}`}
           />
-          <h4><NavLink to={`/airbnb_list/${neighborhood.neighborhood}`}>{neighborhood.neighborhood}</NavLink></h4> 
+          <h4><NavLink to={`/airbnb_list/${neighborhood.neighborhood}`}>{neighborhood.neighborhood}</NavLink></h4>
         </Box>
       )}
     </Container> */}
